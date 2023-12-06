c1133c71bb9ded27365324b90eb47cae22d4f581
Johey Hargrett <joheyhargrett@gmail.com>
2023-12-05 21:47:27 -0500

    completed

--- server/app.py
+++ server/app.py
@@ -1,6 +1,8 @@
 # server/app.py
 #!/usr/bin/env python3
 
+import ipdb
+
 from flask import Flask, make_response
 from flask_migrate import Migrate
 
@@ -21,7 +23,26 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def earthquake_by_id(id):
+    earthquake = Earthquake.query.filter(Earthquake.id == id).first()
+    
+    if earthquake:
+        return make_response(earthquake.to_dict(), 200)
+    else:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
+    
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def earthquakes_minimum_magnitude(magnitude):
+    # magnitude = float(magnitude)
+    earthquakes = Earthquake.query.filter(Earthquake.magnitude >= magnitude).all()
 
+    response_body = {
+        'count': len(earthquakes),
+        'quakes': [quake.to_dict() for quake in earthquakes]
+    }
+
+    return make_response(response_body, 200)
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
